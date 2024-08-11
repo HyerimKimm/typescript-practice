93b09fb6798bfc956c0891cc36c8d90ae11948b0
hyerim <50258232+HyerimKimm@users.noreply.github.com>
2024-08-11 18:32:18 +0900

    섹션2 19. 튜플 작업하기

--- first-project/using-ts.ts
+++ first-project/using-ts.ts
@@ -1,14 +1,12 @@
-const person  = {
+const person   : {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'Maximilian',
   age: 27,
-  hobbies: ['Running', 'Fashion']
+  hobbies: ['Running', 'Fashion'],
+  role: [2, 'author']
 };
 
-let favoriteActivites : string[];
-favoriteActivites = ['Sports'];
-
-console.log(person.name);
-
-for(const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-}
