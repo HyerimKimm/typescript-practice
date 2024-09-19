f7f0ddf0d77671c7c62d1fb1e4376b8a410fdc2c
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2024-09-19 16:29:30 +0900

    interface

--- typescript-class-and-interface/src/app.ts
+++ typescript-class-and-interface/src/app.ts
@@ -1,111 +1,23 @@
-abstract class Department {
-  static fiscalYear = 2024;
-  //   private name: string;
-  protected employees: string[] = [];
+interface Greetable {
+  name: string;
 
-  constructor(protected readonly id: string, private name: string) {
-    // this.id = id;
-    // this.name = n;
+  greet(phrase: string): void;
+}
+
+class Person implements Greetable {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
   }
 
-  static createEmployee(name: string) {
-    return { name: name };
-  }
-
-  abstract describe(this: Department): void;
-
-  addEmployee(employee: string) {
-    this.employees.push(employee);
-  }
-
-  printEmployees(this: Department) {
-    console.log(this.employees);
+  greet(phrase: string) {
+    console.log(`${phrase} ${this.name}`);
   }
 }
 
-class ITDepartment extends Department {
-  constructor(id: string, public admins: string[]) {
-    super(id, "IT");
-    this.admins = admins;
-  }
+let user1: Greetable;
 
-  describe(this: ITDepartment): void {}
-}
+user1 = new Person("Hyerim");
 
-class AccountingDepartment extends Department {
-  private lastReport: string;
-  private static instance: AccountingDepartment;
-
-  get mostRecentReport() {
-    if (this.lastReport) {
-      return this.lastReport;
-    }
-    throw new Error("No report found.");
-  }
-
-  set mostRecentReport(value: string) {
-    if (!value) {
-      throw new Error("Please pass in a valid value!");
-    }
-    this.addReport({ text: value });
-  }
-
-  static getInstance() {
-    if (AccountingDepartment.instance) {
-      return this.instance;
-    } else {
-      this.instance = new AccountingDepartment("d1", []);
-      return this.instance;
-    }
-  }
-
-  private constructor(id: string, private reports: string[]) {
-    super(id, "Accounting");
-    this.lastReport = reports[0]; // 빈 배열이면 undefined가 들어감
-  }
-
-  addEmployee(name: string) {
-    if (name === "Max") {
-      return;
-    }
-    this.employees.push(name);
-  }
-
-  addReport({ text }: { text: string }) {
-    this.reports.push(text);
-    this.lastReport = text;
-  }
-
-  printReports() {
-    console.log(this.reports);
-  }
-
-  describe(this: AccountingDepartment) {
-    console.log("Accounting Department ID :" + this.id);
-  }
-}
-
-const it = new ITDepartment("1", ["Max"]);
-
-it.addEmployee("Hyerim");
-
-console.log(it);
-
-const employee1 = Department.createEmployee("Max");
-console.log(employee1); // { name: 'Max' }
-
-const accounting = AccountingDepartment.getInstance();
-
-accounting.addReport({ text: "Something went wrong..." });
-console.log(accounting.mostRecentReport);
-console.log(Department.fiscalYear);
-
-accounting.mostRecentReport = "Something went wrong...";
-console.log(accounting.mostRecentReport);
-
-accounting.addEmployee("Max");
-accounting.addEmployee("Matu");
-
-// accounting.printEmployees();
-accounting.describe();
-// accounting.printReports();
+user1.greet("Hi! I'm");
