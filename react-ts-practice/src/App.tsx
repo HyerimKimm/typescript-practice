e0ec5ffdeaa985d5145ed7c942565f1086c3f43a
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2024-09-29 16:48:21 +0900

    useRef 타입, useState 타입 정의

--- react-ts-practice/src/App.tsx
+++ react-ts-practice/src/App.tsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
+import NewTodo from './components/NewTodo';
 import TodoList from './components/TodoList';
+import { todo } from './todo.model';
 
 const App: React.FC = () => {
-  const todos = [{ id: 't1', text: 'Finish the course' }];
+  const [todos, setTodos] = useState<todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos(prev => [...prev, { id: Math.random().toString(), text: text }]);
+  };
 
   return (
     <div>
-      <TodoList todos={todos}/>
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList todos={todos} />
     </div>
   );
 };
